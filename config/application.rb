56227066f885e9ceef7a96234ecc3ec94f98a514
EC2 Default User <ec2-user@ip-172-31-45-197.us-east-2.compute.internal>
2018-12-14 11:11:58 +0000

    heroku

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Vod
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,5 +18,5 @@ module Vod
     # the framework and any gems in your application.
   end
   
-  config.assets.initialize_on_precompile = false
+  
 end
